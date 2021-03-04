80d99bc476fb0f872fbc3ca17af7afb45f6b533c
Jean-Guillaume Louis <jguillaume.louis@gmail.com>
2021-03-04 20:08:59 +0100

    Add DIP bank account/authentication service sample

---
+++ src/be/ecam/solid/dip/better/AuthenticationServiceImpl.java
@@ -0,0 +1,9 @@
+package be.ecam.solid.dip.better;
+
+public class AuthenticationServiceImpl implements AuthenticationService {
+
+    public boolean validateToken() {
+        //Some logic
+        return true;
+    }
+}
